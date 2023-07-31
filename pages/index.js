6bfbcbf7fe207c4677b0843cfa72f6512330de86
JoelisKing <adujoel7@gmail.com>
2023-07-31 17:14:10 +0000

    adding link to resume

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Home() {
 								<li>
 									<a
 										className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-										href="#">
+										href="https://drive.google.com/file/d/1tCwhHKsCHcQIbeD5IkkIVINYlcZehKKY/view?usp=sharing">
 										Resume
 									</a>
 								</li>
