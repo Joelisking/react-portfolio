834c708676730934e1c435b65785afc675456d7f
JoelisKing <adujoel7@gmail.com>
2023-07-31 12:57:13 +0000

    fix itr 1

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import Image from "next/image";
 import deved from "../public/dev-ed-wave.png";
 import design from "../public/design.png";
 import code from "../public/code.png";
-import dp from "../public/profile.jpg";
+import dp from "../public/profile.png";
 import consulting from "../public/consulting.png";
 import web1 from "../public/web1.png";
 import web2 from "../public/web2.png";
