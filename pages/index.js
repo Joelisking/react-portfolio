8ca97c9288e926865cc14e8d046bc0c7ef9f6645
JoelisKing <adujoel7@gmail.com>
2023-07-31 17:15:34 +0000

    added target blank

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,8 @@ export default function Home() {
 								<li>
 									<a
 										className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-										href="https://drive.google.com/file/d/1tCwhHKsCHcQIbeD5IkkIVINYlcZehKKY/view?usp=sharing">
+										href="https://drive.google.com/file/d/1tCwhHKsCHcQIbeD5IkkIVINYlcZehKKY/view?usp=sharing"
+										target="__blank">
 										Resume
 									</a>
 								</li>
