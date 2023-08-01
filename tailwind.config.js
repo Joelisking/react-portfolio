49801673f667a63c3aa754f2da2316e1b8b39612
JoelisKing <adujoel7@gmail.com>
2023-08-01 12:22:24 +0000

    final touched, we live

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        burtons: 'burtons',
-      },
-    },
-  },
-  plugins: [],
-};
+	darkMode: ['class'],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			keyframes: {
+				spin: {
+					'100%': {
+						transform: 'rotate(- 360deg)',
+					},
+				},
+			},
+			animation: {
+				spin: 'spin 3s linear infinite',
+			},
+			fontFamily: {
+				burtons: 'burtons',
+        architect: 'architect',
+        poppins: 'poppins',
+			},
+		},
+	},
+	plugins: [],
+}
